16c5b66d9d7370cda654a698dee87deb36dcc68b
Justinas <38762355+ibnzUK@users.noreply.github.com>
2018-06-04 21:41:07 +0100

    just typo
    .nothing here

--- update_witness.sh
+++ update_witness.sh
@@ -6,4 +6,4 @@ read PK
 # repalce in file
 sed  "/PRIVATE_KEY=/c\PRIVATE_KEY=\"${PK}\"" work.sh.bkp > work.sh
 
-echo All done! now staret the server with \"./work.sh start\"
+echo All done! now start the server with \"./work.sh start\"
